188e6a374b3514cef1d0c2460b392207c9ac7c9b
Jeevan Boyeni <jeevanb214@gmail.com>
2020-01-31 17:08:56 +0530

    1)serever port change 2)index page added

---
+++ src/main/java/com/SpringMain/API/AtmSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.SpringMain.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtmSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtmSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SpringMain/API/AtmAppSprinBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringMain.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmAppSprinBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
